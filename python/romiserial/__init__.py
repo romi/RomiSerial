b1694e8160f9f44c8223f18b9d5b7c29731f5d4b
hanappe <peter@hanappe.com>
2025-04-09 20:21:14 +0200

    packiging

---
+++ python/romiserial/__init__.py
@@ -0,0 +1,5 @@
+__version__ = "0.1.0"
+__author__ = 'Peter Hanappe, Doug Boari'
+__credits__ = 'Sony Computer Science Laboratories - Paris'
+
+
---
+++ python/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name='romiserial',
+    version='0.1.0',    
+    description='A library for serial communication (UART) with improved error detection and recovery.',
+    url='https://github.com/romi/RomiSerial',
+    author='Peter Hanappe, Doug Boari',
+    author_email='peter.hanappe@sony.com',
+    license='LGPL v2.1',
+    packages=['romiserial'],
+    install_requires=['crc8'],
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU Lesser General Public License v2 or later (LGPLv2+)',  
+        'Operating System :: POSIX :: Linux',        
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.10',
+    ],
+)
